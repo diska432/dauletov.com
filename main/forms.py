3eaa9a09aa3900de2918cb7a0f38698df30d5925
diska432 <114268737+diska432@users.noreply.github.com>
2023-02-13 21:01:49 -0500

    Create forms.py

---
+++ main/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+class AddressInputForm(forms.Form):
+    address_count = forms.CharField()
+
+    address_string = forms.CharField(label="Please use the following form for inputting addresses", required = False)
+    vehicle_count = forms.CharField()
+
